4e990226193942fd2ea26e36e4d7c5c9118fe9aa
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-10-28 00:00:00 -0500

    Add files via upload
    Primer avance de la aplicación control-clientes

---
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecero',
+  templateUrl: './cabecero.component.html',
+  styleUrls: ['./cabecero.component.css']
+})
+export class CabeceroComponent implements OnInit {
+ // public isCollapsed = true;
+  //public isDropdownCollapsed = true;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
